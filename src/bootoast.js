642d4f276798402df893dde5135bf0fc2d3c0c79
Luiz Filipe Machado Barni <machado@odahcam.com>
2017-11-18 13:08:42 -0200

    beta 2

--- src/bootoast.js
+++ src/bootoast.js
@@ -16,12 +16,13 @@
 // If the 'b' module also uses this type of boilerplate, then
 // in the browser, it will create a global .b that is used below.
 
-;(function(root, factory) {
+;
+(function (root, factory) {
 
 	if (typeof define === 'function' && define.amd) {
 
 		// AMD. Register as an anonymous module.
-		define(['exports', 'jquery'], function(exports, jquery) {
+		define(['exports', 'jquery'], function (exports, jquery) {
 			factory((root.bootoast = exports), jquery);
 		});
 
@@ -37,7 +38,7 @@
 
 	}
 
-}(this, function(exports, $, undefined) {
+}(this, function (exports, $, undefined) {
 	// Use bootoast, bootbox in some fashion.
 
 	'use strict';
@@ -49,115 +50,103 @@
 
 	/**
 	 * Store the plugin name in a variable. It helps you if later decide to change the plugin's name
-	 * @type {string} pluginName
-	 **/
+	 * @var {string} pluginName
+	 */
 	var pluginName = 'bootoast';
 
-	/*
+	/**
 	 * The plugin constructor.
 	 */
 	function Bootoast(options) {
 
-		if (options !== undefined) {
+		if (typeof options === 'undefined') return;
 
-			// Variables default
-			this.settings = $.extend({}, this.defaults);
-
-			// Checa se foi passada uma mensagem flat ou se há opções.
-			if (typeof options !== 'string') {
-				$.extend(this.settings, options);
-			} else {
-				this.settings.message = options;
-			}
-
-			this.content = this.settings.content || this.settings.text || this.settings.message;
-
-			// Define uma posição suportada para o .alert
-			if (this.positionSupported[this.settings.position] === undefined) {
-				// Tenta encontrar um sinônimo
-				var positionCamel = $.camelCase(this.settings.position);
-
-				if (this.positionSinonym[positionCamel] !== undefined) {
-					this.settings.position = this.positionSinonym[positionCamel] || 'bottom-center';
-				}
-			}
-
-			var position = this.settings.position.split('-'),
-				positionSelector = '.' + position.join('.'),
-				positionClass = position.join(' ');
-
-			// Define se o novo .alert deve ser inserido por primeiro ou último no container.
-			this.putTo = position[0] == 'bottom' ? 'appendTo' : 'prependTo';
-
-			// Define o .glyphicon com base no .alert-<type>
-			this.settings.icon = this.settings.icon || this.icons[this.settings.type];
-
-			var containerClass = pluginName + '-container';
-
-			// Checa se já tem container, se não cria um.
-			if ($('body > .' + containerClass + positionSelector).length === 0) {
-				$('<div class="' + containerClass + ' ' + positionClass + '"></div>').appendTo('body');
-			}
-
-			// Adiciona o .alert ao .container conforme seu posicionamento.
-			this.$el = $('<div class="alert alert-' + this.settings.type + ' ' + pluginName + '"><span class="glyphicon glyphicon-' + this.settings.icon + '"></span><span class="bootoast-alert-container"><span class="bootoast-alert-content">' + this.content + '</span></span></div>')[this.putTo]('.' + containerClass + positionSelector);
-
-            var plugin = this;
-
-			if (this.settings.dismissible === true) {
-				this.$el
-                    .addClass('alert-dismissible')
-					.prepend('<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>')
-                    .on('click', 'button.close', function (e) {
-                        e.preventDefault();
-                        plugin.hide();
-                    });
-			}
-
-			// Exibe o .alert
-			this.$el.animate({
-				opacity: 1,
-			}, this.settings.animationDuration);
-
-			// Se o .alert tem tempo de expiração
-			if (!!this.settings.timeout) {
-				var secondsTimeout = parseInt(this.settings.timeout * 1000),
-					timer = this.hide(secondsTimeout),
-					plugin = this;
-
-                if (this.settings.timeoutProgress) {
-                    this.timeoutProgress = this.insertTimeoutProgress(this.settings.timeoutProgress);
-                }
-
-				// Pausa o timeout baseado no hover
-				this.$el.hover(
-					clearTimeout.bind(window, timer),
-					function() {
-						timer = plugin.hide(secondsTimeout);
-					}
-                );
-			}
+		if (typeof options === 'string') {
+			options = {
+				message: options
+			};
 		}
-	};
+
+		// define as opções interpretadas
+		this.settings = $.extend({}, this.defaults, options);
+		// define o conteúdo
+		this.content = this.settings.content || this.settings.text || this.settings.message;
+		// define o elemento de progress como nulo
+		this.timeoutProgress = null;
+		// define uma posição aceitável pro elemento
+		this.position = this.positionFor(this.settings.position).split('-');
+		// Define o .glyphicon com base no .alert-<type>
+		this.settings.icon = this.settings.icon || this.icons[this.settings.type];
+
+		var containerClass = pluginName + '-container';
+
+		this.containerSelector = '.' + containerClass + '.' + this.position.join('.');
+
+		// Checa se já tem container, se não cria um.
+		if ($('body > ' + this.containerSelector).length === 0) {
+			$('<div>', {
+				class: containerClass + ' ' + this.position.join(' ')
+			}).appendTo('body');
+		}
+
+		// Adiciona o .alert ao .container conforme seu posicionamento.
+		this.$el = $('<div class="' + pluginName + ' alert alert-' + this.typeFor(this.settings.type) + '"><span class="glyphicon glyphicon-' + this.settings.icon + '"></span><span class="bootoast-alert-container"><span class="bootoast-alert-content">' + this.content + '</span></span></div>');
+
+		this.init();
+	}
 
 	$.extend(Bootoast.prototype, {
-		/*
+		/**
 		 * Default options
-		 * @type {Object} defaults
+		 *
+		 * @var {Object} defaults
 		 */
 		defaults: {
-			message: 'Helo!', // String: HTML
-			type: 'info', // String: ['warning', 'success', 'danger', 'info']
-			position: 'bottom-center', // String: ['top-left', 'top-center', 'top-right', 'bottom-left', 'bottom-center', 'bottom-right']
-			icon: undefined, // String: name
-			timeout: 3, // seconds, use null to disable timeout hidin'
-            timeoutProgress: false, // [false, 'top', 'bottom', 'background']
-			animationDuration: 300, // Int: animation duration in miliseconds
+			/**
+			 * Any HTML string.
+			 * @var {string}
+			 */
+			message: 'Bootoast!',
+			/**
+			 * ['warning', 'success', 'danger', 'info']
+			 * @var {string}
+			 */
+			type: 'info',
+			/**
+			 * ['top-left', 'top-center', 'top-right', 'bottom-left', 'bottom-center', 'bottom-right']
+			 * @var {string}
+			 */
+			position: 'bottom-center',
+			/**
+			 * @var {string}
+			 */
+			icon: undefined,
+			/**
+			 * Seconds, use null to disable timeout hiding.
+			 * @var {int}
+			 */
+			timeout: 3,
+			/** 
+			 * [false, 'top', 'bottom', 'background']
+			 * 
+			 * @var {string|bool}
+			 */
+			timeoutProgress: false,
+			/**
+			 * Animation duration in miliseconds.
+			 * 
+			 * @var {int}
+			 */
+			animationDuration: 300,
+			/**
+			 * @var {bool}
+			 */
 			dismissible: true,
 		},
-		/*
+		/**
 		 * Default icons
-		 * @type {Object} icons
+		 *
+		 * @var {Object} icons
 		 */
 		icons: {
 			warning: 'exclamation-sign',
@@ -165,9 +154,45 @@
 			danger: 'remove-sign',
 			info: 'info-sign'
 		},
-		/*
+		/**
+		 * Types
+		 *
+		 * @var {Object} types
+		 */
+		types: [
+			'primary',
+			'secondary',
+			'info',
+			'success',
+			'warning',
+			'danger'
+		],
+		/**
+		 * Type Sinonymus
+		 *
+		 * @var {Object} typeSinonym
+		 */
+		typeSinonym: {
+			warn: 'warning',
+			error: 'danger',
+		},
+		/**
+		 * Position Supported
+		 *
+		 * @var {array} positions
+		 */
+		positions: [
+			'top-left',
+			'top-center',
+			'top-right',
+			'bottom-left',
+			'bottom-center',
+			'bottom-right'
+		],
+		/**
 		 * Position Sinonymus
-		 * @type {Object} positionSinonym
+		 *
+		 * @var {Object} positionSinonym
 		 */
 		positionSinonym: {
 			bottom: 'bottom-center',
@@ -177,70 +202,154 @@
 			rightTop: 'top-right',
 			leftTop: 'top-left'
 		},
-		/*
-		 * Position Supported
-		 * @type {array} positionSupported
-		 */
-		positionSupported: [
-			'top-left',
-			'top-center',
-			'top-right',
-			'bottom-left',
-			'bottom-center',
-			'bottom-right'
-		],
 		/**
-		 * @type {method} hide
-		 * @param {int} timeout
-		 * @return {int} setTimeoutID The setTimeout ID.
-		 **/
-		hide: function(timeout) {
+		 * Initializes the plugin functionality
+		 */
+		init: function () {
+
+			// Define se o novo .alert deve ser inserido por primeiro ou último no container.
+			this.$el[(this.position[0] == 'bottom' ? 'append' : 'prepend') + 'To'](this.containerSelector);
+
 			var plugin = this;
-			return setTimeout(function() {
+
+			if (this.settings.dismissible === true) {
+				this.$el
+					.addClass('alert-dismissible')
+					.prepend('<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>')
+					.on('click', 'button.close', function (e) {
+						e.preventDefault();
+						plugin.hide();
+					});
+			}
+
+			// Exibe o .alert
+			this.$el.animate({
+				opacity: 1,
+			}, this.settings.animationDuration);
+
+			// Se o .alert tem tempo de expiração
+			if (!!this.settings.timeout) {
+
+				var secondsTimeout = parseInt(this.settings.timeout * 1000);
+
+				this.hide(secondsTimeout);
+			}
+		},
+		/**
+		 * @method hide
+		 *
+		 * @param {int} timeout
+		 *
+		 * @return {int} The setTimeout ID.
+		 */
+		hide: function (timeout) {
+			var plugin = this;
+
+			if (this.settings.timeoutProgress) {
+				this.setTimeoutProgress(this.settings.timeoutProgress);
+			}
+
+			var timer = setTimeout(function () {
 				plugin.$el.animate({
 					opacity: 0,
-				}, plugin.settings.animationDuration, function() {
+				}, plugin.settings.animationDuration, function () {
 					plugin.$el.remove();
 				});
 			}, timeout || 0);
+
+			// Pausa o timeout baseado no hover
+			this.$el.hover(
+				clearTimeout.bind(window, timer),
+				function () {
+					timer = plugin.hide(timeout);
+				}
+			);
 		},
-        /**
-         * @param {string} progressPosition
-         */
-        insertTimeoutProgress: function(progressPosition) {
+		/**
+		 * @param {string} progressPosition
+		 */
+		setTimeoutProgress: function (progressPosition) {
 
-                var progressPutToAllowed = {
-                    'top': 'prepend',
-                    'bottom': 'append',
-                };
-                var $progress = $('div', {
-                    class: 'progress',
-                    html: $('<div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuemin="0" aria-valuenow="0" aria-valuemax="100">')
-                });
-                var progressPutTo = progressPutToAllowed[progressPosition];
-                var progressPositionClass = typeof progressPutTo === 'string' ? progressPosition : 'background';
+			if (this.timeoutProgress !== null) {
+				this.timeoutProgress.remove();
+			}
 
-                return $progress.addClass(progressPositionClass)[progressPut + 'To'](this.$el);
-        }
+			var positionOptions = {
+				top: 'prepend',
+				bottom: 'append',
+			};
+
+			var $progress = $('<div>', {
+				class: 'progress',
+				html: $('<div>', {
+					class: 'progress-bar progress-bar-striped active',
+					role: 'progressbar',
+					'aria-valuemin': 0,
+					'aria-valuenow': 0,
+					'aria-valuemax': 100,
+				})
+			});
+
+			var putMethod = positionOptions[progressPosition] || 'append';
+			var position = typeof positionOptions[progressPosition] === 'string' ? progressPosition : 'background';
+
+			this.timeoutProgress = $progress.addClass('progress-' + position)[putMethod + 'To'](this.$el)
+
+			return this.timeoutProgress;
+		},
+		/**
+		 * @param {string} type
+		 *
+		 * @return Gets the correct type-name for the given value or null.
+		 */
+		typeFor: function (type) {
+
+			// se esta type é padrão
+			if (this.types[type] !== undefined) return type;
+
+			if (!type) return 'default';
+
+			return this.typeSinonym[type] || type;
+		},
+		/**
+		 * @param {string} position
+		 *
+		 * @return Gets the correct position-name for the given value or ''.
+		 */
+		positionFor: function (position) {
+
+			// se esta posição é padrão
+			if (this.positions[position] !== undefined) return position;
+
+			var positionCamel = $.camelCase(position);
+
+			// Tenta encontrar um sinônimo
+			return this.positionSinonym[positionCamel] || 'bottom-center';
+		},
 	});
 
-    function moveProgressbar(elem, qty) {
-        var width = 100;
-        return setInterval(function () {
-            if (width <= 0) {
-                clearInterval(id);
-            } else {
-                width--;
-                elem.style.width = width + '%';
-            }
-        }, 100/qty);
-    }
+	/**
+	 *
+	 * @param {HTMLElement} elem
+	 * @param {int} qty
+	 */
+	function moveProgressbar(elem, qty) {
+		var width = 100;
+		return setInterval(function () {
+			if (width <= 0) {
+				clearInterval(id);
+			} else {
+				width--;
+				elem.style.width = width + '%';
+			}
+		}, 100 / qty);
+	}
 
 	// attach properties to the exports object to define
 	// the exported module properties.
-	exports.toast = function(options) {
+	exports.toast = function (options) {
 		return new Bootoast(options);
-	};;
+	};
 
 	return exports;
 }));
